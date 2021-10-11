83f0ca00cab5b54adb119158ec48758d2b189b36
QA <vqa031@gmail.com>
2021-10-11 10:22:15 +0700

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c0895c991fc238eaa4eb5a6685debbe1'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '880c69ac-dacb-40dc-b1ae-510b2b75336c'
+};
